b45e3db5bc9f6a0336dfd52ee669b0112b6f957c
Florencia Accastelli <f.accastelli@gmail.com>
2024-06-05 12:44:35 -0300

    functionalities for mobile

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const hamburgerMenu = document.querySelector('#hamburger-menu');
-const navbar = document.querySelector('#navbar')
-const closeBtn = document.querySelector('#close-btn')
+const navbar = document.querySelector('#navbar');
+const closeBtn = document.querySelector('#close-btn');
+const featuresBtn = document.querySelector('#features-btn');
+const companyBtn = document.querySelector('#company-btn')
+const featuresUl = document.querySelector('#features-ul');
+const companyUl = document.querySelector('#company-ul');
 
 hamburgerMenu.addEventListener('click', () => {
     navbar.style.display = 'block';
@@ -8,4 +12,12 @@ hamburgerMenu.addEventListener('click', () => {
 
 closeBtn.addEventListener('click', () => {
     navbar.style.display = 'none';
+})
+
+featuresBtn.addEventListener('click', () => {
+    featuresUl.style.display = (featuresUl.style.display === 'block') ?'none' : 'block';
+})
+
+companyBtn.addEventListener('click', ()=> {
+    companyUl.style.display = (companyUl.style.display === 'block') ?'none' : 'block';
 })
