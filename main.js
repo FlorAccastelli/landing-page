213073da186a8e0e57c4667fe618ded0cb5628fb
Florencia Accastelli <f.accastelli@gmail.com>
2024-06-05 14:43:40 -0300

    dynamic arrow

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const featuresBtn = document.querySelector('#features-btn');
 const companyBtn = document.querySelector('#company-btn')
 const featuresUl = document.querySelector('#features-ul');
 const companyUl = document.querySelector('#company-ul');
+const featuresBtnArrow = document.querySelector('#features-btn-arrow');
+const companyBtnArrow = document.querySelector('#company-btn-arrow')
 
 hamburgerMenu.addEventListener('click', () => {
     navbar.style.display = 'block';
@@ -12,17 +14,21 @@ hamburgerMenu.addEventListener('click', () => {
 
 closeBtn.addEventListener('click', () => {
     const screenSize = window.innerWidth;
-    if(screenSize >= 768) {
-        navbar.style.display = 'block';
-    } else {
+    if(screenSize < 768) {
         navbar.style.display = 'none';
+    } else {
+        navbar.style.display = 'block';
     }
 })
 
 featuresBtn.addEventListener('click', () => {
     featuresUl.style.display = (featuresUl.style.display === 'block') ?'none' : 'block';
+    featuresBtnArrow.style.transition = 'transform 0.3s ease'
+    featuresBtnArrow.style.transform =(featuresBtnArrow.style.transform === 'rotate(180deg)') ? 'rotate(0deg)' : 'rotate(180deg)'
 })
 
 companyBtn.addEventListener('click', ()=> {
     companyUl.style.display = (companyUl.style.display === 'block') ?'none' : 'block';
+    companyBtnArrow.style.transition = 'transform 0.3s ease'
+    companyBtnArrow.style.transform =(companyBtnArrow.style.transform === 'rotate(180deg)') ? 'rotate(0deg)' : 'rotate(180deg)'
 })
